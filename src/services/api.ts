6a9c9f3dc79551c3b16b90de58f74bd1c369b029
Monu01123 <monumeena0112@gmail.com>
2025-11-18 10:27:57 +0530

    maintainence

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import { Sweet, AuthResponse, Cart, Order, Bill, RevenueData, DashboardStats } from '../types';
 import { tokenStorage } from '../utils/token';
 
-const API_BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:3001/api';
+const API_BASE_URL = "https://incubyte-assignment23.onrender.com";
 
 export const authApi = {
   async register(email: string, password: string, fullName: string): Promise<AuthResponse> {
